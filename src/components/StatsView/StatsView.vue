b3ff31a231d38fafc4aed4de16da73d949f104aa
Philippe Moore <philippealmoore@gmail.com>
2017-11-05 15:13:10 -0500

    stats changes

---
+++ src/components/StatsView/StatsView.vue
@@ -0,0 +1,29 @@
+<template>
+</template>
+
+<script>
+import * as firebase from 'firebase'
+import StatsChart from '@/components/StatsView/StatsChart'
+export default {
+  data () {
+    return {
+      title: 'Stats'
+    }
+  },
+  created () {
+
+  },
+  components: {
+    StatsChart
+  },
+  methods: {
+    getStatsData () {
+      firebase.database().
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
