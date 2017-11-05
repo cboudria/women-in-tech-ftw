cecf8766dc2e009c93745af9a33238acc4346f9e
Philippe Moore <philippealmoore@gmail.com>
2017-11-05 12:17:02 -0500

    cleanup errors, wrap router in v-content

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     >
       <v-list>
         <v-list-tile
-          value="true"
+          value="false"
           v-for="(item, i) in items"
           :key="i"
         >
@@ -21,15 +21,17 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
+    <v-toolbar fixed app >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
     <main>
-      <router-view></router-view>
+      <v-content>
+        <router-view></router-view>
+      </v-content>
     </main>
-    <v-footer :fixed="fixed" app>
+    <v-footer fixed app>
       <span>&copy; 2017</span>
     </v-footer>
   </v-app>
@@ -39,7 +41,7 @@
   export default {
     data () {
       return {
-        drawer: true,
+        drawer: false,
         items: [{
           icon: 'bubble_chart',
           title: 'Inspire'
